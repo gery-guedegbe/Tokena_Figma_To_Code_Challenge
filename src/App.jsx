f92e82e76c871095fd7c0a2839432eb850772caf
gery-guedegbe <guedegbe2003@gmail.com>
2024-08-23 14:56:30 +0100

    Updates For Deploy on Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layouts from "./components/Layouts";
 import DashboardSection from "./sections/DashboardSection";
 import NewsSection from "./sections/NewsSection";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/news" element={<NewsSection />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
